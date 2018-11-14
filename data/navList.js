07e691ab4b1b7d742b84eb43c332343bf69571e4
shagu <1243933010@qq.com>
2018-11-14 21:44:53 +0800

    data: 新增该文件
    Signed-off-by: shagu <1243933010@qq.com>

---
+++ data/navList.js
@@ -0,0 +1,28 @@
+var navList = [
+    {
+        name: '系统管理',
+        navArr: ['组织机构(G)', '系统参数(S)', '操作杂志(L)', '事件备份(B)', '事件恢复(R)', '数据库管理(D)', '切换数据库', '修改密码(P)'],
+        navSlideImg: ['organization', 'systemParameter', '0perationMagazine', 'eventBackup', 'eventRecovery', 'databaseManagement', 'switchDatabase', 'changePassword']
+    },
+    {
+        name: '控制器管理',
+        navArr: ['控制器(C)', '临时时间组(T)', '特殊时间组(S)', '校正时间(K)', '门参数(D)', '电梯管理(E)', '点参数(P)', '联动组配置(L)'],
+        navSlideImg: ['controller','temporaryTimeGroup','specialTimeGroup','correctionTime ','doorParameter','elevatorManagement','pointParameter','linkGroupConfiguration']
+    },
+    {
+        name: '权限管理',
+        navArr: ['部门管理', '人员管理', '门禁权限组', '人员卡管理中心', '操作员管理', '二级管理员权限分配'],
+        navSlideImg: ['departmentManagement','personnelManagement','accessControlGroup','personnelCardManagementCenter','operatorManagement','secondaryAdministratorAuthorityAssignment','','']
+    },
+    {
+        name: '实时监控',
+        navArr: ['事件监控(V)', '事件查询(Q)', '进出统计(S)', '门状态控制(D)', '点控制(P)', '区域设置(Z)', '联动组控制(L)'],
+        navSlideImg: ['eventMonitoring','eventQuery','incomingStatistics','doorStatusControl','pointControl','regionalSetting','linkageGroupControl']
+    },
+    {
+        name: '防盗报警',
+        navArr: ['报警参数', '防区参数', '报警模式', '操作参数', '点状态计划', '下载设置'],
+        navSlideImg: ['alarmParameters','zoneParameters','alarmMode','operationParameters','pointStatusPlan','downloadSettings']
+    }
+            
+];
