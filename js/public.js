a8685023e488f8db325fa5ba7d3ca70c70e43260
Yanlan Shen <yanlan.shen.@rosinson.com>
2018-11-14 18:06:48 +0800

    js: 封装函数, 提高复用率
    Signed-off-by: Yanlan Shen <yanlan.shen.@rosinson.com>

---
+++ js/public.js
@@ -0,0 +1,33 @@
+(function(win, doc, $){
+
+    $(function(){
+        $(win).resize(function(){
+            $('section').height($(win).height());
+            $('section .sec .sec-container').outerWidth($('section .sec').width() - $('section .sec nav').outerWidth());
+        })
+        $('section').height($(win).height());
+        $('section .sec .sec-container').outerWidth($('section .sec').width() - $('section .sec nav').outerWidth());
+
+        var firTmp = '';
+        for(var i=0;i<navList.length;i++){
+            var secTmp = '';
+            for(var j=0;j<navList[i].navArr.length;j++){
+                secTmp += `
+                <li class="sec-slide">
+                    <img src="${navList[i].navSlideImg[j]}.png">
+                    <a href="./organization.html">${navList[i].navArr[j]}</a>
+                </li>
+                `;
+            }
+            firTmp += `
+            <li class="fir-menu">
+                <a class="fir-slide" href="javascript:;">${navList[i].name}</a>
+                <ul>${secTmp}</ul>
+            </li>
+            `;
+        }
+        $('section nav > ul').html(firTmp);
+        
+    });
+
+})(window, document, jQuery);
