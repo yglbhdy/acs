df74f806f0475cb13a777ea833da790c76c330ba
Yue <1243933010@qq.com>
2018-11-20 11:07:41 +0800

    完成门参数界面
    Signed-off-by: Yue <1243933010@qq.com>

--- data/dooParameterList.js
+++ data/dooParameterList.js
@@ -6,24 +6,106 @@ var organiza = [
        content:['1086','1086','1086','1086']
    },
    {
-    name:'1F南侧大厅',
+    name:'2F南侧大厅',
     img:'oriimg',
-    content:['1086','1086','1086','1086']
+    content:['1087','1087','1087','1087']
    },
    {
-    name:'1F南侧大厅',
+    name:'3F南侧大厅',
     img:'oriimg',
-    content:['1086','1086','1086','1086']
+    content:['1088','1088','1088','1088']
    },
    {
-    name:'1F南侧大厅',
+    name:'4F南侧大厅',
     img:'oriimg',
-    content:['1086','1086','1086','1086']
-   },
-   {
-    name:'1F南侧大厅',
-    img:'oriimg',
-    content:['1086','1086','1086','1086']
+    content:['1089','1089','1089','1089']
    }
+]
 
+//门的类型
+var theTypeOfTheDoor = [
+    {
+        content:['单向门','双向门','单向门']
+    }
+]
+
+//区域APB 
+var regionalAPB = [
+{content:['APB 无效','APB 有效']}
+]
+
+//首卡开门
+var openingTheFirstCard = [
+    {content:['有效','无效']}
+]
+
+//多卡组合开门
+var multiCardCombinationDoor = [
+    {content:['有效','无效']}
+]
+
+//读卡器类型
+var cardReaderType = [
+    {content:['自动检测','被动检测']}
+]
+
+//LED极性
+var LED_Polarity = [
+    {content:['阴极','阳极']}
+]
+
+//非法卡进入
+var illegalCardAccess = [
+    {content:['普通消息','普通消息','普通消息']}
+]
+
+//有效卡进入
+var validCardAccess = [
+    {content:['普通消息','普通消息','普通消息']}
+]
+
+//在无效时段
+var invalidPeriod = [
+    {content:['普通消息','普通消息','普通消息']}
+]
+
+//无效的区域
+var invalidArea = [
+    {content:['普通消息','普通消息','普通消息']}
+]
+
+//用户密码错误
+var userPasswordError = [
+    {content:['普通消息','普通消息','普通消息']}
+]
+
+//无效的门点
+var invalidDoorPoint = [
+    {content:['普通消息','普通消息','普通消息']}
+]
+
+//用户跟随入
+var userFollower = [
+    {content:['普通消息','普通消息','普通消息']}
+]
+
+//其他错误
+var otherMistakes = [
+    {content:['普通消息','普通消息','普通消息']}
+]
+
+
+//table表格的th数据
+// var tableThData = [
+//     {content:['&nbsp;&nbsp;&nbsp;','序号','时间','新状态','临时','特殊日期组','正常日期组']}
+// ]
+
+//table表格的第一个td数据
+var tableTdOneData = [
+    {content:['T1','T2','S1','S2','S3','S4','S5','S6','S7','日','一','二','三','四','五','六']}
+]
+
+//table表格的第二个数据
+var tableTdTwoData = [
+    {content:['','xx','xx','xx','xx','xx','xx','xx','xx','xx','xx','xx','xx','xx','xx',]}
 ]
