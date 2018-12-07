a63f0dde052b3a1b286b5d7466ca6fcc4c98a4e6
Yue <1243933010@qq.com>
2018-12-07 17:24:42 +0800

    控制器报警参数界面完成
    Signed-off-by: Yue <1243933010@qq.com>

---
+++ data/alarmParametersList.js
@@ -0,0 +1,44 @@
+var table = [
+    ['通讯节点','控制器名称','地址'],
+    ['门禁控制主机','1F南侧大厅','1'],
+    ['门禁控制主机','1F南侧大厅','1'],
+    ['门禁控制主机','1F南侧大厅','1'],
+    ['门禁控制主机','1F南侧大厅','1'],
+    ['门禁控制主机','1F南侧大厅','1'],
+    ['门禁控制主机','1F南侧大厅','1'],
+    ['门禁控制主机','1F南侧大厅','1'],
+    ['门禁控制主机','1F南侧大厅','1'],
+    ['门禁控制主机','1F南侧大厅','1'],
+    ['门禁控制主机','1F南侧大厅','1'],
+    ['门禁控制主机','1F南侧大厅','1'],
+    ['门禁控制主机','1F南侧大厅','1'],
+    ['门禁控制主机','1F南侧大厅','1'],
+    ['门禁控制主机','1F南侧大厅','1'],
+    ['门禁控制主机','1F南侧大厅','1'],
+    
+]
+
+var rapidDeployment = ['有效','无效'];
+
+var effectiveCardOpeningDoor = ['有效','无效'];
+
+
+var clothWithdrawalControlPoint = ['【1-1】号门门磁','【1-2】号门门磁','【1-3】号门门磁'];
+
+var clothWithdrawalPoint = ['【1-3】号门门磁','【1-2】号门门磁','【1-1】号门门磁'];
+
+var alarmOutputPoint = ['【1-3】号门门磁','【1-2】号门门磁','【1-1】号门门磁'];
+
+var alarmTypeMode = ['【1-1】号门门磁','【1-2】号门门磁','【1-3】号门门磁'];
+
+var alarmModule = ['有效','无效','有效'];
+
+var clothWithdrawalPoint_two = ['有效','无效','有效'];
+
+var clothWithdrawalPoint_three = ['有效','无效','有效'];
+
+var bypassDefenseArea = ['有效','无效','有效'];
+
+var redLightFlashesDuringDeployment = ['有效','无效','有效'];
+
+var greenLightFlashesWhenClothingDelayOccurs = ['有效','无效','有效'];
---
+++ js/alarmParameters.js
@@ -0,0 +1,88 @@
+$(function(){
+
+
+
+    var btn = $('.alarmParameters_right>ul li');
+    var divs = $('.alarmParameters_right>div');
+    $.each(btn,function(i,e){
+        divs.eq(0).remove('none')
+        $(this).click(function(){
+            divs.eq(i).removeClass('none').siblings('div').addClass('none');
+            $(this).css('border','none').siblings('li').css('border','1px solid #868686');
+        })
+    })
+
+
+
+    var tableData = '';
+    table.forEach(function(element,index){
+        var tableX = '';
+        element.forEach(function(e,i){
+            if(index == 0){
+                tableX +=`<th>${e}</th>`
+            }else{
+                tableX +=`<td>${e}</td>`
+            }
+        })
+        tableData +=`<tr>${tableX}</tr>`;
+    })
+    $('.alarmParameters_left table').html(tableData)
+
+
+
+
+
+
+
+    //快速布防
+    mua(rapidDeployment,'.rapidDeployment')
+
+
+    //有效卡开门
+    mua(effectiveCardOpeningDoor,'.effectiveCardOpeningDoor')
+
+
+
+    //布撤防控制点
+    mua(clothWithdrawalControlPoint,'.clothWithdrawalControlPoint')
+
+
+
+    //布撤防指示点
+    mua(clothWithdrawalPoint,'.clothWithdrawalPoint')
+
+
+
+    //报警输出点
+    mua(alarmOutputPoint,'.alarmOutputPoint')
+
+
+    //报警类型模式
+    mua(alarmTypeMode,'.alarmTypeMode')
+
+
+
+
+    //报警模块
+    mua(alarmModule,'.alarmModule')
+
+
+
+    //布撤防输入点
+    mua(clothWithdrawalPoint_two,'.clothWithdrawalPoint_two')
+
+
+   
+   //布撤防输出点
+    mua(clothWithdrawalPoint_three,'.clothWithdrawalPoint_three')
+    
+
+    //旁路防区
+    mua(bypassDefenseArea,'.bypassDefenseArea')
+
+    //布防时红灯闪
+    mua(redLightFlashesDuringDeployment,'.redLightFlashesDuringDeployment')
+
+    //布防延迟时绿灯闪
+    mua(greenLightFlashesWhenClothingDelayOccurs,'.greenLightFlashesWhenClothingDelayOccurs')
+})
