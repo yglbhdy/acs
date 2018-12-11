f546d952f226ed4fab67a1775b6f69db4730dfe3
Yanlan Shen <yanlan.shen.@rosinson.com>
2018-12-11 15:46:25 +0800

    卡片管理中心界面完成
    Signed-off-by: Yanlan Shen <yanlan.shen.@rosinson.com>

--- data/personnelCardManagementCenterData.js
+++ data/personnelCardManagementCenterData.js
@@ -1,35 +1,3 @@
-var PCMC = [
-    {
-        name: '卡片注册',
-        className: 'registered',
-        data: [
-            ['卡区号', '卡起始号', '卡数量', '登记时间'],
-            ['ajsdio', 'hasui', 'ashduoi', 'ashdui']
-        ],
-        subBtn: ['重新下载','增加', '删除']
-    },
-    {
-        name: '分配卡片',
-        className: 'distribution',
-        data: []
-    },
-    {
-        name: '用户卡授权',
-        className: 'authorization',
-        data: []
-    },
-    {
-        name: '用户卡管理',
-        className: 'management',
-        data: []
-    },
-    {
-        name: '资料下载管理',
-        className: 'download',
-        data: []
-    }
-];
-
 // 卡片注册 卡片登记情况
 var registered = [
     ['卡区号', '卡起始号', '卡数量', '登记时间'],
@@ -52,4 +20,29 @@ var distribution = [
 var distributionData = [
     ['卡区号', '卡号'],
     ['asgdiu', 'kashydo']
+];
+
+// 用户卡授权  未授权
+var unauthorized = [
+    ["用户编号", "姓名", "性别", "部门", "职位", "区号", "卡号", "有效期", "权限组", "个性化权限", "卡类型", "所属卡类型"],
+    ['ashdi', 'ashdo', 'asihdo', 'asjdio', 'ishdoa', 'asihdo', 'asuhdo', 'akshd', 'ishdoa', 'asihdo', 'asuhdo', 'akshd']
+];
+
+// 用户卡授权  已经授权
+var authorized = [
+    ["用户编号", "姓名", "性别", "部门", "职位", "区号", "卡号", "有效期", "权限组", "个性化权限", "卡类型", "所属卡类型"],
+    ['ashasddi', 'ashdo', 'asihasddo', 'asjdiasdo', 'ishasddoa', 'asihdo', 'asuhdo', 'akshd', 'ishdoa', 'asiasdhdo', 'asuhdasdo', 'akasdshd']
+];
+
+// 用户卡管理
+var management = [
+    ["用户编号", "姓名", "性别", "部门", "区号", "卡号"],
+    ['ashasddi', 'ashdo', 'asihasddo', 'asjdiasdo', 'ishasddoa', 'asihdo'],
+    ['ashasddi', 'ashdo', 'asihasddo', 'asjdiasdo', 'ishasddoa', 'asihdo']
+];
+
+// 资料下载管理
+var download = [
+    ["编号", "姓名", "部门", "区号", "卡号", "通讯节点", "控制器", "控制类型", "门一权限", "门二权限", "门三权限", "门四权限"],
+    ['ashasddi', 'ashdo', 'asihasddo', 'asjdiasdo', 'ishasddoa', 'asihdo', 'asuhdo', 'akshd', 'ishdoa', 'asiasdhdo', 'asuhdasdo', 'akasdshd']
 ];
--- js/personnelCardManagementCenter.js
+++ js/personnelCardManagementCenter.js
@@ -1,5 +1,6 @@
 (function(win, doc, $){
 
+    // 选项卡切换
     $('.tabs span').click(function(){
         var className = $(this).attr('class');
         $('.tabs span').removeClass('active');
@@ -15,6 +16,22 @@
                 $(elem).addClass('active');
             }
         });
+    });
+
+    // 未授权列表和已授权列表切换
+    $('.authorization .selectBtn input').change(function(){
+        if(this.checked == true) {
+            var thisID = $(this).attr('id');
+            $('.top-content .table-container .list').each(function(index, elem){
+                var elemClass = $(elem).attr('class');
+
+                if(elemClass.indexOf(thisID) != (-1)){
+            console.log(thisID, '++', elemClass, '++', elemClass.indexOf(thisID));
+                    $('.top-content .table-container .list').removeClass('active');
+                    $(elem).addClass('active');
+                }
+            });
+        } 
     });
 
     function tableData(data, tmp){
@@ -45,7 +62,21 @@
     // 分配卡片 右边
     tableData(distributionData, '.distribution .top-content .right-content .table-container table');
     
-    $('.fixed-bottom tr td:nth-child(3)').text(registered.length - 1);
+    // 用户卡授权  未授权
+    tableData(unauthorized, '.top-content .table-container .unauthorized');
+
+    // 用户卡授权  已授权
+    tableData(authorized, '.top-content .table-container .authorized');
+
+    // 用户卡管理
+    tableData(management, '.management .top-content .table-container table');
+
+    // 资料下载管理
+    tableData(download, '.download .top-content .table-container table');
+
+    $('.registered .fixed-bottom tr td:nth-child(3)').text(registered.length - 1);
     $('.op-face .top-content > p span').text(registered.length - 1);
+    $('.management .fixed-bottom tr td:nth-child(2)').text(management.length - 1);
+    $('.download .fixed-bottom tr td:nth-child(2)').text(download.length - 1);
 
 })(window, document, jQuery);
