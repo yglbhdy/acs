7e1fa63aefc9bb5aa859bcf57b51a7ab88900709
shagu <1243933010@qq.com>
2018-12-18 17:24:30 +0800

    页面:完成downloasetting页面
    Signed-off-by: shagu <1243933010@qq.com>

---
+++ js/downloadSettings.js
@@ -0,0 +1,22 @@
+$(function(){
+
+var tableData = '';
+table.forEach(function(e,i){
+    var tableX = '';
+    e.forEach(function(ele,ind){
+        if(i == 0){
+            tableX +=`<th>${ele}</th>`
+        }else{
+            tableX +=`<td>${ele}</td>`
+        }
+    })
+    tableData +=`<tr>${tableX}</tr>`
+})
+
+$('.downloadSettings table').html(tableData);
+
+
+
+
+
+})
