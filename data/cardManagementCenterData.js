3abaed64ab52a8230c0424b3c2d19f624127ce49
Yanlan Shen <yanlan.shen.@rosinson.com>
2018-12-18 18:51:35 +0800

    卡管理界面完成
    Signed-off-by: Yanlan Shen <yanlan.shen.@rosinson.com>

---
+++ data/cardManagementCenterData.js
@@ -0,0 +1,53 @@
+// 部门选择
+var departmentSelection = [
+    ['用户ID', '用户名'],
+    ['ashasf', 'ashos']
+];
+
+// 部门成员
+var departmentMember = [
+    ['用户ID', '用户名'],
+    ['ashasf', 'ashos']
+];
+
+// 权限信息条
+var permissionInformation = [
+    ["卡区号", "卡号", "权限组", "门名称", "控制器名称", "时间组名称"],
+    ['ashasf', 'ashos','ashasf', 'ashos','ashasf', 'ashos']
+];
+
+// 权限组交叉权限
+var permissionGroupCrossPermission = [
+    ['门名称', '控制器名称'],
+    ['ashasf', 'ashos']
+];
+
+// 个性化授权
+var personalizedAuthorization = [
+    ["预操作信息", '操作结果', "用户名", "权限组", "卡区号", "卡号", "门名称", "控制器名称", "通讯节点"],
+    ['ashasf', 'ashos','ashasf', 'ashos','ashasf', 'ashos', 'ashasf', 'ashos','ashasf']
+];
+
+// 权限组授权
+var authorityGroupAuthorization = [
+    ["预操作信息", '操作结果', "用户名", "权限组", "卡区号", "卡号", "门名称", "控制器名称", "通讯节点"],
+    ['ashasf', 'ashos','ashasf', 'ashos','ashasf', 'ashos', 'ashasf', 'ashos','ashasf']
+];
+
+// 拥有的权限
+var ownedPermissions = [
+    ['组ID', '权限组名称'],
+    ['ashasf', 'ashos']
+];
+
+// 可选择的权限组
+var selectablePermissionGroup = [
+    ['组ID', '权限组名称'],
+    ['ashasf', 'ashos']
+];
+
+// 权限组(包商临时组)包含的门点
+var doorPoint = [
+    ['门名称', '控制器名称'],
+    ['ashasf', 'ashos']
+];
--- js/cardManagementCenter.js
+++ js/cardManagementCenter.js
@@ -1,5 +1,42 @@
 (function(win, doc, $){
 
-    
+    function tableData(data, tmp){
+        var tr = '';
+        data.forEach(function(elem, index){
+            var td = '';
+            elem.forEach(function(e, i){
+                td += `<td>${e}</td>`;
+            });
+            tr += `<tr>${td}</tr>`;
+        });
+        $(tmp).html(tr);
+    }
+
+    // 部门选择
+    tableData(departmentSelection, '.left-content .left-container .bottom-content .table-container table');
+
+    // 部门成员
+    tableData(departmentMember, '.left-content .right-container .bottom-content .table-container table');
+
+    // 权限信息条
+    tableData(permissionInformation, '.center-content .top-content .left-container .table-container table');
+
+    // 权限组交叉权限
+    tableData(permissionGroupCrossPermission, '.center-content .top-content .right-container .table-container table');
+
+    // 个性化授权
+    tableData(personalizedAuthorization, '.center-content .center-container .table-container table');
+
+    // 权限组授权
+    tableData(authorityGroupAuthorization, '.center-content .bottom-content .table-container table');
+
+    // 拥有的权限
+    tableData(ownedPermissions, '.right-content .top-content .table-container table');
+
+    // 可选择的权限组
+    tableData(selectablePermissionGroup, '.right-content .center-container .table-container table');
+
+    // 权限组(包商临时组)包含的门点
+    tableData(doorPoint, '.right-content .bottom-content .table-container table');
 
 })(window, document, jQuery);
