3a94354290404189f19c31f22f7009f05587b1d0
shagu <1243933010@qq.com>
2018-12-18 17:22:44 +0800

    页面：完成pointStatusPlan页面
    Signed-off-by: shagu <1243933010@qq.com>

--- data/pointStatusPlanList.js
+++ data/pointStatusPlanList.js
@@ -28,3 +28,17 @@ var pointStatusPlanningGroup = [
     ['xx','xx','xx','xx','xx','xx','xx','xx','xx'],
 
 ]
+
+var choicePoint = ['布撤防状态点','布撤防状态点','布撤防状态点'];
+
+var effectivePoint = ['有效','无效'];
+
+var pointState = ['全部撤防','局部撤防'];
+
+var continuity = ['默认','默认'];
+
+var normalDateGroup = ['星期日','星期一','星期二','星期三','星期四','星期五','星期六'];
+
+var specialDateGroup = ['S1','S2','S3','S4','S5','S6','S7'];
+
+var temporaryDateGroup = ['T1','T2'];
--- js/pointStatusPlan.js
+++ js/pointStatusPlan.js
@@ -25,5 +25,64 @@ $(function(){
         pointStatusPlanningGroupText+=`<tr>${pointStatusPlanningGroupData}</tr>`
     })
     $('.pointStatusPlanningGroup').after(pointStatusPlanningGroupText)
- 
+
+
+//模态框的点击显示消失
+    $('.add_btn').click(function(){
+        console.log('x')
+        $('.mask').css('display','block');
+    })
+
+ $('.signOut').click(function(){
+     $('.mask').css('display','none');
+ })
+
+
+
+ //模态框内的选择点的数据遍历
+ mua(choicePoint,'.choicePoint');
+
+ //点有效数据遍历
+mua(effectivePoint,'.effectivePoint');
+
+//点状态数据遍历
+mua(pointState,'.pointState');
+
+//持续情况数据遍历
+mua(continuity,'.continuity');
+
+
+//正常日期组的数据遍历
+var normalDateGroupData = '';
+for(var i = 0;i<normalDateGroup.length;i++){
+    normalDateGroupData+=`
+    <li>
+    <input type="checkbox" />
+    <label for="">${normalDateGroup[i]}</label>
+    </li>`
+}
+$('.normalDateGroup ul').html(normalDateGroupData);
+
+//特殊日期组的数据遍历
+var specialDateGroupData = '';
+for(var i = 0;i<specialDateGroup.length;i++){
+    specialDateGroupData+=`
+    <li>
+    <input type="checkbox" />
+    <label for="">${specialDateGroup[i]}</label>
+    </li>`
+}
+$('.specialDateGroup ul').html(specialDateGroupData);
+
+
+var temporaryDateGroupData = '';
+
+for(var i = 0;i<temporaryDateGroup.length;i++){
+    temporaryDateGroupData+=`
+    <li>
+    <input type="checkbox" />
+    <label for="">${temporaryDateGroup[i]}</label>
+    </li>`
+}
+$('.temporaryDateGroup ul').html(temporaryDateGroupData);
  })
