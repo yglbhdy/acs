6b37c2f82a0f9b7750fef84dafd0885c02041ce8
shagu <1243933010@qq.com>
2018-12-20 12:14:43 +0800

    add：新写点控制页面
    Signed-off-by: shagu <1243933010@qq.com>

---
+++ data/incomingStatisticsList.js
@@ -0,0 +1,46 @@
+var organiza = [
+    {
+        name:'1F南侧大厅',
+        img:'oriimg',
+        content:['1086','1086','1086','1086']
+    },
+    {
+     name:'2F南侧大厅',
+     img:'oriimg',
+     content:['1087','1087','1087','1087']
+    },
+    {
+     name:'3F南侧大厅',
+     img:'oriimg',
+     content:['1088','1088','1088','1088']
+    },
+    {
+     name:'4F南侧大厅',
+     img:'oriimg',
+     content:['1089','1089','1089','1089']
+    },
+    {
+     name:'4F南侧大厅',
+     img:'oriimg',
+     content:['1089','1089','1089','1089']
+    },
+    {
+     name:'4F南侧大厅',
+     img:'oriimg',
+     content:['1089','1089','1089','1089']
+    },
+    {
+     name:'4F南侧大厅',
+     img:'oriimg',
+     content:['1089','1089','1089','1089']
+    },
+    {
+     name:'4F南侧大厅',
+     img:'oriimg',
+     content:['1089','1089','1089','1089']
+    }
+ ]
+ 
+
+//门状态select数据
+ var gateState = ['安全','不安全'];
